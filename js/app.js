4ecd8415c98c4c6d81acbfaa6c8604ade3b8f64c
mitchgemma <mitchgemma@gmail.com>
2022-02-13 19:06:47 -0500

    fixed adding multiple p elements & added turn message

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-const gameBoard = document.getElementById('#game-board')
+
 const reset = document.getElementById('#reset')
 const tile1 = document.getElementById('tile1')
 const container = document.getElementById('container')
+let message = document.getElementById('message')
 
 // A user should be able to click on different squares to make a move.
 
@@ -13,7 +14,7 @@ const container = document.getElementById('container')
 //     tile1.appendChild(newX)
 // })
 
-let lastMove = "X";
+let lastMove = "X"
 
 // loops through child divs of the container
 for (let i = 0; i < container.childNodes.length; i++){
@@ -21,17 +22,36 @@ for (let i = 0; i < container.childNodes.length; i++){
     // console.log(tile)
 //  event listener that creates p element for x or 0
     tile.addEventListener ('click', () =>  {
-        const newNode = document.createElement ('p');
+        // if a tile has a child node, do not let it create another p element
+        if (tile.childNodes.length === 0){
+            let newNode = document.createElement ('p');
+        
+
         //test displayed within newNode
-        newNode.innerHTML = "X"
+        newNode.innerHTML = lastMove
         // need to append new node the the correct tile
         tile.appendChild(newNode)
         // now I need to make alternations between Xs and 0s
         if(lastMove === "X"){
-            
+            //add "0" to the board
+            lastMove = "0"
+            newNode = "0"
+            message.innerHTML = "X just picked. It is 0's turn."
+        }else {
+            //add "X" back to the board
+            lastMove = "X"
+            newNode =  "X"
+            message.innerHTML = "0 just picked. It is X's turn."
         }
-    })
     }
+    const clearBoard = () =>
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+    })
+}
+
+    
 
 
 
@@ -52,10 +72,3 @@ for (let i = 0; i < container.childNodes.length; i++){
 // Detect draw conditions (ties/cat's game)
 // Detect winner: Stop game and declare the winner if one player ends up getting three in a row.
 // Hint: Determine a set of winning combinations. Check those combinations on the board contents after every move.
-
-
-
-
-    
-
-
