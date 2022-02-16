7826094b9ec4a11b912f0813b78f83897598dd5e
mitchgemma <mitchgemma@gmail.com>
2022-02-16 16:45:02 -0500

    Winning Boards Working

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const reset = document.getElementById('reset')
 const container = document.getElementById('container')
 let message = document.getElementById('message')
 let finalMessage = document.getElementById('final-message')
-
 // A user should be able to click on different squares to make a move.
 
     
@@ -22,58 +21,167 @@ let finalMessage = document.getElementById('final-message')
                 
                 
                 //test displayed within newNode
-                newNode.innerHTML = lastMove
+                newNode.textContent = lastMove
                 // need to append new node the the correct tile
                 tile.appendChild(newNode)
                 
+                checkWin()
                 // now I need to make alternations between Xs and 0s
                 if(lastMove === "X"){
                     //add "0" to the board
                     lastMove = "0"
                     // newNode = "0"
-                    message.innerHTML = "X just picked. It is 0's turn."
+                    message.textContent = "X just picked. It is 0's turn."
                 }else {
                     //add "X" back to the board
                     lastMove = "X"
                     // newNode =  "X"
-                    message.innerHTML = "0 just picked. It is X's turn."
+                    message.textContent = "0 just picked. It is X's turn."
                 }
             }
         })
-        //function to reset the board
-        const clearBoard = () => {
-            while (tile.firstChild){
-                tile.removeChild (tile.firstChild)
+        // determine winning boards
+        // ========================================================================================
+        const checkWin = () => {
+            const tile1 = document.getElementById("tile1");
+            const tile2 = document.getElementById("tile2");
+            const tile3 = document.getElementById("tile3");
+            const tile4 = document.getElementById("tile4");
+            const tile5 = document.getElementById("tile5");
+            const tile6 = document.getElementById("tile6");
+            const tile7 = document.getElementById("tile7");
+            const tile8 = document.getElementById("tile8");
+            const tile9 = document.getElementById("tile9");
+            
+
+
+
+            // const value1 = tile1.firstChild.textContent;
+            // const value2 = tile2.firstChild.textContent;        
+            // const value3 = tile3.firstChild.textContent;
+            // const value4 = tile4.firstChild.textContent;
+            // const value5 = tile5.firstChild.textContent;
+            // const value6 = tile6.firstChild.textContent;
+            // const value7 = tile7.firstChild.textContent;
+            // const value8 = tile8.firstChild.textContent;
+            // const value9 = tile9.firstChild.textContent;
+            console.log("checking for win")
+            if (tile1.firstChild && tile2.firstChild && tile3.firstChild){
+                const value1 = tile1.firstChild.textContent;
+                const value2 = tile2.firstChild.textContent;        
+                const value3 = tile3.firstChild.textContent;
+            
+            if (value1 === "X" && value2 === "X" && value3 === "X"){
+                finalMessage.innerHTML = "X wins!";
+            } else if (value1 === "0" && value2 === "0" && value3 === "0"){
+                finalMessage.innerHTML = "0 wins!";
             }
+            ;}
+            if (tile4.firstChild && tile5.firstChild && tile6.firstChild){
+                
+                const value4 = tile4.firstChild.textContent;
+                const value5 = tile5.firstChild.textContent;
+                const value6 = tile6.firstChild.textContent;
+                    if (value4 === "X" && value5 === "X" && value6 === "X"){
+                        finalMessage.innerHTML = "X wins!";
+            } else if (value4 === "0" && value5 === "0" && value6 === "0"){
+                finalMessage.innerHTML = "0 wins!";
+        }
+            }
+            if (tile7.firstChild && tile8.firstChild && tile9.firstChild){
+                const value7 = tile7.firstChild.textContent;
+                const value8 = tile8.firstChild.textContent;
+                const value9 = tile9.firstChild.textContent;
+
+            if (value7 === "X" && value8 === "X" && value9 === "X"){
+                finalMessage.innerHTML = "X wins!";
+            } else if (value7 === "0" && value8 === "0" && value9 === "0"){
+                finalMessage.innerHTML = "0 wins!";
+        }
+        }
+        //now for all the vertical winning combinations
+
+            if (tile1.firstChild && tile4.firstChild && tile7.firstChild){
+                const value1 = tile1.firstChild.textContent;
+                const value4 = tile4.firstChild.textContent;
+                const value7 = tile7.firstChild.textContent;
+
+            if (value1 === "X" && value4 === "X" && value7 === "X"){
+                finalMessage.innerHTML = "X wins!";
+            } else if (value1 === "0" && value4 === "0" && value7 === "0"){
+                finalMessage.innerHTML = "0 wins!";
+            }
+        }
+
+            if (tile2.firstChild && tile5.firstChild && tile8.firstChild){
+                const value2 = tile2.firstChild.textContent; 
+                const value5 = tile5.firstChild.textContent;
+                const value8 = tile8.firstChild.textContent;
+            
+                if (value2 === "X" && value5 === "X" && value8 === "X"){
+                    finalMessage.innerHTML = "X wins!";
+                } else if (value2 === "0" && value5 === "0" && value8 === "0"){
+                    finalMessage.innerHTML = "0 wins!";
+                }
+            }
+
+            if (tile3.firstChild && tile6.firstChild && tile9.firstChild){
+                const value3 = tile3.firstChild.textContent; 
+                const value6 = tile6.firstChild.textContent;
+                const value9 = tile9.firstChild.textContent;
+            
+                if (value3 === "X" && value6 === "X" && value9 === "X"){
+                    finalMessage.innerHTML = "X wins!";
+                } else if (value3 === "0" && value6 === "0" && value9 === "0"){
+                    finalMessage.innerHTML = "0 wins!";
+                }
+            }
+            
+            if (tile1.firstChild && tile5.firstChild && tile9.firstChild){
+                const value1 = tile1.firstChild.textContent; 
+                const value5 = tile5.firstChild.textContent;
+                const value9 = tile9.firstChild.textContent;
+            
+                if (value1 === "X" && value5 === "X" && value9 === "X"){
+                    finalMessage.innerHTML = "X wins!";
+                } else if (value1 === "0" && value5 === "0" && value9 === "0"){
+                    finalMessage.innerHTML = "0 wins!";
+                }
+            }
+
+            if (tile3.firstChild && tile5.firstChild && tile7.firstChild){
+                const value3 = tile3.firstChild.textContent; 
+                const value5 = tile5.firstChild.textContent;
+                const value7 = tile7.firstChild.textContent;
+            
+                if (value3 === "X" && value5 === "X" && value7 === "X"){
+                    finalMessage.innerHTML = "X wins!";
+                } else if (value3 === "0" && value5 === "0" && value7=== "0"){
+                    finalMessage.innerHTML = "0 wins!";
+                }
+            }
+
+
+
+
+            //function to reset the board
+            
+            const clearBoard = () => {
+                while (tile.firstChild){
+                tile.removeChild (tile.firstChild)
+                //remove text in p
+            }
+            message.textContent = " "
         }
         reset.addEventListener('click', clearBoard)
     }
-    
-    // not best way to go about game ending - was not able to spend a lot of time
-    
-//     const tile1 = document.getElementById("tile1");
-//     const tile2 = document.getElementById("tile2");
-//     const tile3 = document.getElementById("tile3");
-//     const tile4 = document.getElementById("tile4");
-//     const tile5 = document.getElementById("tile5");
-//     const tile6 = document.getElementById("tile6");
-//     const tile7 = document.getElementById("tile7");
-//     const tile8 = document.getElementById("tile8");
-//     const tile9 = document.getElementById("tile9");
+}
 
-
-// if (tile1.innerHTML === "X" && tile2.innerHTML === "X" && tile3.innerHTML === "X" || tile1 === "0" && tile2 === "0" && tile3 === "0"){
-//     finalMessage.innerHTML = "X wins!"
-// }
+    //1 determine winning outcomes
+    //2 remove p text
+    //3 end game - tiles can't be clicked
     
-    
-    
-    
-    
-    
-    
-    
-    
+    //disableTiles()
     
     
     // Every click will alternate between marking an X and O
